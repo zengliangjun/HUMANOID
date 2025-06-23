4d11529307677830f71a68015c9cc7f7aaef64d9
liangjunzeng <liangjunzeng@163.com>
2025-06-23 11:21:53 +0800

    #Feature clean code

--- source/tasks/g1_12dof/episode_status/env_covar_cfg.py
+++ source/tasks/g1_12dof/episode_status/env_covar_cfg.py
@@ -2,7 +2,7 @@ from isaaclab.utils import configclass
 from ..mdps  import obs
 from . import env_cfg
 
-from isaaclabex.envs.mdp.statistics import joints
+from isaaclabex.envs.mdp.statistics import joints #, body
 from isaaclabex.envs.managers import term_cfg
 from isaaclab.managers import SceneEntityCfg, ObservationTermCfg, ObservationGroupCfg
 from isaaclab.utils.noise import AdditiveUniformNoiseCfg as Unoise
@@ -26,6 +26,15 @@ class StatisticsCfg:
             "asset_cfg": SceneEntityCfg("robot")},
         export_interval = 1000000
     )
+    """
+    footclearance = term_cfg.StatisticsTermCfg(
+        func= body.StatusFootClearance,
+        params={
+            "asset_cfg": SceneEntityCfg("robot",
+                     body_names=[".*left_ankle_roll_link", ".*right_ankle_roll_link"])},
+        export_interval = 1000000
+    )
+    """
 
 @configclass
 class ObservationsCfg(obs.ObservationsCfg):
@@ -34,19 +43,19 @@ class ObservationsCfg(obs.ObservationsCfg):
     class ActionStatisticsCfg(ObservationGroupCfg):
         action_episode_mean = ObservationTermCfg(func=statistics.obs_episode_mean,
                                        params={"pos_statistics_name": "action"},
-                                       noise=Unoise(n_min=-0.01, n_max=0.01))
+                                       noise=Unoise(n_min=-0.1, n_max=0.1))
         action_episode_variance = ObservationTermCfg(func=statistics.obs_episode_variance,
                                        params={"pos_statistics_name": "action"},
-                                       noise=Unoise(n_min=-0.01, n_max=0.01))
+                                       noise=Unoise(n_min=-0.1, n_max=0.1))
         action_step_mean_mean = ObservationTermCfg(func=statistics.obs_step_mean_mean,
                                        params={"pos_statistics_name": "action"},
-                                       noise=Unoise(n_min=-0.01, n_max=0.01))
+                                       noise=Unoise(n_min=-0.1, n_max=0.1))
         action_step_mean_variance = ObservationTermCfg(func=statistics.obs_step_mean_variance,
                                        params={"pos_statistics_name": "action"},
-                                       noise=Unoise(n_min=-0.01, n_max=0.01))
+                                       noise=Unoise(n_min=-0.1, n_max=0.1))
         action_step_variance_mean = ObservationTermCfg(func=statistics.obs_step_variance_mean,
                                        params={"pos_statistics_name": "action"},
-                                       noise=Unoise(n_min=-0.01, n_max=0.01))
+                                       noise=Unoise(n_min=-0.1, n_max=0.1))
 
         def __post_init__(self):
             self.enable_corruption = True
